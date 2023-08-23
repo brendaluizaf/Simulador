92eb3aefdbeff942e8823a88f32b8e16d6e57a7f
brendaluizaf <brendaluizaf4@gmail.com>
2023-08-23 11:17:23 -0300

    Update trab2.java

--- trab2.java
+++ trab2.java
@@ -1,102 +1,100 @@
 package Simulador;
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class trab2 {
-    public static void main(String[] args) { /* Método principal */
-        String[] listaPalavras = { "CACAU", "PEIXE", "MACARRAO", "CHOCOLATE", "RABANETE", "RISOTO" }; /*
-                                                                                                       * Lista do array
-                                                                                                       */
-        Random random = new Random(); /* Biblioteca que permite gerar dados aleatórios */
-        Scanner entrada = new Scanner(System.in);/* Permite a inserção dos dados pelo usuário */
-        int pontos = 0; /* Váriavel indicativa da forma como iniciará a contagem dos pontos */
+    public static void main(String[] args) { //Método principal
+        String[] listaPalavras = { "CACAU", "PEIXE", "MACARRAO", "CHOCOLATE", "RABANETE", "RISOTO" };
+        Random random = new Random();
+        Scanner entrada = new Scanner(System.in);
+
+        int[] pontuacoes = new int[10]; // Vetor para armazenar pontuações
+        String[] nomes = new String[10]; // Vetor para armazenar nomes
+        int indice = 0; // Índice para inserção de pontuações e nomes
+        
+        System.out.print("Digite a quantidade de jogadores: ");
+        int numJogadores = entrada.nextInt();
+        entrada.nextLine(); // Limpa o buffer de entrada
+        
+        for (int jogador = 0; jogador < numJogadores; jogador++) {
+            System.out.print("Digite o nome do jogador " + (jogador + 1) + ": ");
+            String nome = entrada.nextLine();
+            nomes[jogador] = nome;
+        }
+
         boolean jogarNovamente = true;
-        String nome = "";
-        System.out.println("1-Iniciar");
-        System.out.println("2-Verificar pontos");
-        System.out.println("3-Sair");
-        System.out.print("Escolha uma opção:");
+        
+        while (true) { // Loop infinito para o menu principal
+            System.out.println("1-Iniciar");
+            System.out.println("2-Verificar pontos");
+            System.out.println("3-Sair");
+            System.out.print("Escolha uma opção:");
 
-        int opcoes = entrada.nextInt();/* escolha das opções do switch case */
+            int opcoes = entrada.nextInt();
 
-        switch (opcoes) {
-            case 1:
-                System.out.println("Digite seu nome: ");
-                nome = entrada.next();
-                // System.out.println("Iniciar novo jogo");/* opção 1 iniciar o jogo */
+            switch (opcoes) {
+                case 1:
+                    for (int jogador = 0; jogador < numJogadores; jogador++) {
+                        jogarNovamente = true; // Reinicia a variável a cada jogador
+                        System.out.println("\nJogador " + nomes[jogador]);
 
-                while (jogarNovamente) {
-                    String palavraSecreta = listaPalavras[random.nextInt(listaPalavras.length)];
-                    int vidas = 6;
-                    char[] letrasDescobertas = new char[palavraSecreta.length()];
-                    for (int i = 0; i < letrasDescobertas.length; i++) {/* Loop que permite descoberta das letras */
-                        letrasDescobertas[i] = '_';
-                    }
+                        String palavraSecreta = listaPalavras[random.nextInt(listaPalavras.length)];
+                        int vidas = 6;
+                        char[] letrasDescobertas = new char[palavraSecreta.length()];
+                        for (int i = 0; i < letrasDescobertas.length; i++) {
+                            letrasDescobertas[i] = '_';
+                        }
 
-                    while (vidas > 0) { /* permite que o usuário tenha tentativas para descobrir as letras */
-                        System.out.println("Palavra: " + new String(letrasDescobertas));
-                        System.out.println("Vidas restantes: " + vidas);
-                        System.out.print("Digite uma letra: ");
-                        char letra = entrada.next().toUpperCase().charAt(0);/* Deixa a letra maiúscula */
+                        while (vidas > 0) { // permite que o usuário tenha tentativas para descobrir as letras 
+                            System.out.println("Palavra: " + new String(letrasDescobertas));
+                            System.out.println("Vidas restantes: " + vidas);
+                            System.out.print("Digite uma letra: ");
+                            char letra = entrada.next().toUpperCase().charAt(0);// Deixa a letra maiúscula 
 
-                        boolean letraEncontrada = false;
-                        for (int i = 0; i < palavraSecreta.length(); i++) {
-                            if (palavraSecreta.charAt(i) == letra) {
-                                letrasDescobertas[i] = letra;
-                                letraEncontrada = true;/* Descoberta da letra correta preencherá o espaço */
+                            boolean letraEncontrada = false;
+                            for (int i = 0; i < palavraSecreta.length(); i++) {
+                                if (palavraSecreta.charAt(i) == letra) {
+                                    letrasDescobertas[i] = letra;
+                                    letraEncontrada = true;// Descoberta da letra correta preencherá o espaço
+                                }
+                            }
+
+                            if (!letraEncontrada) {
+                                vidas--;
+                                System.out.println("Letra não encontrada. Vidas restantes: " + vidas);//Se letra errada perde uma vida
+                            }
+
+                            if (new String(letrasDescobertas).equals(palavraSecreta)) {
+                                System.out.println("Parabéns! Você acertou a palavra: " + palavraSecreta);
+                                pontuacoes[jogador]++; // Incrementa a pontuação
+                                break;
                             }
                         }
-                        if (letra > 1) {
-                            System.out.println("Digite apenas uma letra.");
-                        }
 
-                        if (!letraEncontrada) {
-                            vidas--;
-                            System.out.println("Letra não encontrada. Vidas restantes: " + vidas);/*
-                                                                                                   * Se letra errada
-                                                                                                   * perde uma vida
-                                                                                                   */
-                        }
+                        if (vidas == 0) {
+                            System.out.println("Você perdeu! A palavra era: " + palavraSecreta);
+                        } // Se o número de tentativas exceder perde o jogo 
 
-                        if (new String(letrasDescobertas).equals(palavraSecreta)) {
-                            System.out.println("Parabéns! Você acertou a palavra: " + palavraSecreta);
-                            pontos++; /* A palavra completa corretamente retorna o aviso */
-                            break;
-                        }
+                        System.out.print("Deseja jogar novamente? (S/N): ");
+                        String resposta = entrada.next().toUpperCase();
+                        jogarNovamente = resposta.equals("S");
                     }
-
-                    if (vidas == 0) {
-                        System.out.println("Você perdeu! A palavra era: " + palavraSecreta);
-                    } /* Se o número de tentativas exceder perde o jogo */
-
-                    System.out.print("Deseja jogar novamente? (S/N): ");
-                    String resposta = entrada.next().toUpperCase();
-                    jogarNovamente = resposta.equals("S"); /* Opção de o jogador tentar novamente */
-                }
-                break;
-            case 2:
-                System.out.println(" Verificar pontuações");
-                System.out.println(pontos);/* verifica a pontuação feita */
-                break;
-            case 3:
-                System.out.println("Sair do jogo.");/* sai do jogo e mostra quantos pontos foram feitos */
-                jogarNovamente = false;
-                break;
-            default:
-                System.out.println("\nOpção inválida! Escolha um dos itens abaixo.");
-                System.out.println("1-Iniciar");
-                System.out.println("2-Verificar pontos");
-                System.out.println("3-Sair");
+                    break;
+                case 2:
+                    System.out.println("Verificar pontuações");
+                    for (int i = 0; i < numJogadores; i++) {
+                        System.out.println("Nome: " + nomes[i] + " | Pontuação: " + pontuacoes[i]);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Sair do jogo.");
+                    entrada.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção inválida! Escolha um dos itens abaixo.");
+                    break;
+            }
         }
-        if (pontos == 0 || nome == null || nome == "") {
-            System.out.println("Você ainda não jogou ou não obteve nenhuma pontuação.");
-        } else {
-            System.out.println("Obrigado por jogar! Você conseguiu o total de : " + pontos + " ponto(s) " + nome);
-            // System.out.printf("O jogador %s fez %d pontos.\nObrigado por jogar!", nome,
-            // pontos);
-        }
-        entrada.close(); /* Se a opção de continuar for negada pelo usuário emitira o comunicado */
     }
-
 }
